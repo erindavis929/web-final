e17ca7d68eb53188180c52e3ae21ec502327c78d
Erin Davis <erindavis929@outlook.com>
2017-06-09 20:35:00 -0400

    Initial

---
+++ app/controllers/index.js
@@ -0,0 +1,25 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  schools: Ember.A([
+    {
+      image:"ailogo.jpeg",
+      name:"Art Institute of Indianapolis",
+      time:"July 2016 - Present",
+      description:"I am studying torwards a Bachelors Degree in Graphic and Web Design with a focus on Web Design. I am also leaarning coding so I can also code a website not just design them."
+    },
+    {
+      image:"ailogo.jpeg",
+      name:"Art Institute of Indianapolis",
+      time:"July 2016 - Present",
+      description:"I am studying torwards a Bachelors Degree in Graphic and Web Design with a focus on Web Design. I am also leaarning coding so I can also code a website not just design them."
+    },
+    {
+      image:"ailogo.jpeg",
+      name:"Art Institute of Indianapolis",
+      time:"July 2016 - Present",
+      description:"I am studying torwards a Bachelors Degree in Graphic and Web Design with a focus on Web Design. I am also leaarning coding so I can also code a website not just design them."
+    }
+  ])
+
+});
